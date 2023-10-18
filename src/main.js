371684113078cf34eb566ec53705e6596d17dd14
Darius Goore <darius@dariuss-mbp-2.myfiosgateway.com>
2023-10-17 21:38:02 -0400

    save progress with new Question template

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 import App from './App.vue'
 
-createApp(App).use(createPinia()).mount('#app')
+createApp(App)
+    .use(createPinia())
+    .use(Buefy)
+    .mount('#app')
